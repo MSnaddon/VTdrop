e8e543a05e33fbb1dba9e44a17a50b67cf395d35
Michael Snaddon <msnaddon0991@gmail.com>
2016-12-13 14:54:06 +0000

    final prototype of player.js

--- client/src/reducers/player.js
+++ client/src/reducers/player.js
@@ -1,15 +1,18 @@
 export default function player(state = {}, action){
+
+	// use Object.create()??? use eval to work on the clone.
+	// THIS IS NOT A FINAL SOLUTION, this is simply a owrking prototype.
+	//Really need to think this solution out. Running code on an object may require considerable fannying about with testing and deciding what code ACTUALLY needs to be returned and used in the action command.
+	let player = JSON.parse(JSON.stringify(state));
+	
 	switch (action.type){
 		case 'RUNCODE':
-			// use Object.create()??? use eval to work on the clone.
-			// THIS IS NOT A FINAL SOLUTION, this is simply a owrking prototype.
-			//Really need to think this solution out. Running code on an object may require considerable fannying about with testing and deciding what code ACTUALLY needs to be returned and used in the action command.
-			
-			// does not work with "let", works with "var"... WHYY!YY!Y!Y!Y!Y!Y!
-			var player = JSON.parse(JSON.stringify(state))
 			eval(action.consoleInput)
 			return player
+		default:
+			return state
 	}
 
-	return state
+	
 }
+
